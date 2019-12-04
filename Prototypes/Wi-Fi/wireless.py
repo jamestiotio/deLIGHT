ffa651436c67c2ff124291890d5bc06391694a67
James R T <jamestiotio@gmail.com>
2019-12-04 16:02:49 +0800

    Implement servo control for Wi-Fi script

--- Prototypes/Wi-Fi/wireless.py
+++ Prototypes/Wi-Fi/wireless.py
@@ -3,6 +3,7 @@
 # Created by James Raphael Tiovalen (2019)
 
 # Import libraries
+import utime
 import network
 import machine
 
@@ -15,15 +16,22 @@ gc.collect()
 # Define Wi-Fi Access Point SSID & Password
 ap_ssid = 'deLIGHTer'
 ap_password = ''
+authorized_users = [b'']  # Extend this to non-hardcoded list
 
 # Define Wi-Fi Station SSID & Password
 sta_ssid = ''
 sta_password = ''
 
 # Define PWM variables for servo control
-# Duty for servo is between 40 - 115
-pin = machine.Pin(4)
+# Duty for servo is between 41 - 120, but mileage might differ
+pin = machine.Pin(26)
 servo = machine.PWM(pin, freq=50)
+LID_CLOSE = 120
+LID_OPEN = 41
+servo_state = 'CLOSED'
+
+# Define button for closing servo
+button = machine.Pin(37, machine.Pin.IN)
 
 
 def station():
@@ -57,19 +65,62 @@ def access_point():
     print('Access point created!')
     print(ap.ifconfig())  # Returns (ip, subnet/netmask, gateway, dns) as a tuple
     
+    global servo_state
+
+    # Define AP internal functions
+    def wait_for_release():
+        while True:
+            if button.value() == 1:
+                return
+
+    
+    def button_check():
+        global servo_state
+        while True:
+            if servo_state == 'OPEN':
+                servo.duty(LID_CLOSE)
+                if not ap.isconnected():
+                    print('Authorized device disconnected! Closing...')
+                else:
+                    print('Main button pressed! Closing...')
+                servo_state = 'CLOSED'
+                return
+
+
+    def device_scan():
+        global servo_state
+        while True:
+            if button.value() == 0 and ap.isconnected():  # TODO: Add condition to pass to integrated finger module and do fingerprint method if ap.isconnected() == false
+                for i in range(len(ap.status('stations'))):
+                    print('Got a connection from %s.' % str(ap.status('stations')[i][0]))
+                    
+                    if ap.status('stations')[i][0] in authorized_users and servo_state == 'CLOSED':
+                        # Duty for servo is between 41 - 120, but mileage might differ
+                        servo.duty(LID_OPEN)
+                        servo_state = 'OPEN'
+                        return
+    
+    
     # Main access point loop
+    # TODO: Add LCD screen text indicators
+    # M5Stack module is not available in the current firmware used for the M5 stick
+    # Need to fetch the Lobo MicroPython build
     while True:
-        if ap.isconnected():
-            for i in range(len(ap.status('stations'))):
-                print('Got a connection from %s.' % str(ap.status('stations')[i][0]))
-                
-                # if ap.status('stations')[i][0] == b'':
-                    # Duty for servo is between 40 - 115
-                    # servo.duty(100)
+        while True:
+            if button.value() == 0 and servo_state == 'CLOSED':
+                device_scan()
+                wait_for_release()
+                break
+
+        while True:
+            if (servo_state == 'OPEN') and (button.value() == 0 or not ap.isconnected()):
+                button_check()
+                wait_for_release()
+                break
 
 
 def main():
-    station()  # TODO: Need to forward network packet data to the Internet and back to corresponding devices so that we can use ESP32 as Wi-Fi Repeater as well to maintain Internet access for connected stations.
+    # station()  # TODO: Need to forward network packet data to the Internet and back to corresponding devices so that we can use ESP32 as Wi-Fi Repeater as well to maintain Internet access for connected stations.
     access_point()
 
 
