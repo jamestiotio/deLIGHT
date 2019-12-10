de95e1dd0cf00ea6235bb336e807abbd0325fa02
Ragul Balaji <ragulbaby@gmail.com>
2019-12-10 21:58:18 +0800

    sound!

--- Prototypes/Wi-Fi and Biometric/mystick.py
+++ Prototypes/Wi-Fi and Biometric/mystick.py
@@ -1,2 +1,9 @@
 from lib import time_ex
 timEx = time_ex.TimerEx()
+
+from speaker import Speaker
+spkr = Speaker()
+spkr.setVolume(100)
+
+for n in [(2349, 250),(2959, 250),(3520, 250),(4698, 500)]:
+	spkr.tone(n[0],n[1],100)
