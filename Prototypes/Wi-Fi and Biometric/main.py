9a85e65246e9d6bb4eed47e65f9622dc41da8403
Ragul Balaji <ragulbaby@gmail.com>
2019-12-13 02:50:19 +0800

    enrollment

--- Prototypes/Wi-Fi and Biometric/main.py
+++ Prototypes/Wi-Fi and Biometric/main.py
@@ -1,10 +1,20 @@
 # DELIGHT BIOMETRIC UPY
 # RAGULBALAJI 2019
 
-from m5stack import lcd, buttonA
+from m5stack import lcd, buttonA, buttonB
 from random import randint, getrandbits
 from time import sleep_ms
 from machine import Pin, PWM, reset
+import json
+
+def readConfig():
+	config = json.load(open('lighter.json'))
+	return config
+	
+def writeConfig(config): # Use responsibly
+	cfile = open('lighter.json','w')
+	json.dump(config, cfile)
+	cfile.close()
 
 TONE_DELIGHT = [(1174, 250), (1479, 250), (1760, 250), (2349, 500)]
 TONE_DENY = [(1760, 100), (1, 50), (1760, 400)]
@@ -37,26 +47,18 @@ sleep_ms(100)
 tone(TONE_DELIGHT)
 sleep_ms(500)
 
-def mainScrn():
-	clrscrn()
-	lcd.setTextColor(lcd.RED)
-	lcd.print("deLIGHTer\n")
-	lcd.setTextColor(lcd.WHITE)
-	lcd.print("Scan Finger to Unlock")
-mainScrn()
-
 
 GAME_RUNNING = False
 def game_start():
 	lcd.font(lcd.FONT_DejaVu18)
 	GAME_RUNNING = True
-	HEALTH = 20
+	HEALTH = 10
 	bird = [80,20,0,0,15] # y,x,vy,vx,r
 	blns = [[80,-10,0,-1,15,lcd.BLUE],[40,-10,0.2,-0.5,10,lcd.GREEN]] # y,x,vy,vx,r,color
 	GRAV = -1
 	lastbtn = False
 	while GAME_RUNNING:
-		if HEALTH < 0: break
+		if HEALTH < 1: break
 		if buttonA.isPressed() and not lastbtn: bird[2] += 15
 		lastbtn = buttonA.isPressed()
 		bird[2] += GRAV
@@ -90,12 +92,18 @@ def game_start():
 		sleep_ms(30)
 	
 	lcd.setTextColor(lcd.WHITE)
-	lcd.print("GAME\nOVER")
-	while not buttonA.isPressed(): sleep_ms(10)
+	lcd.text(40,0,"GAME")
+	lcd.text(20,0,"OVER")
+	sleep_ms(700)
+	tone(TONE_DENY)
+	reset()
 
 from _finger import Finger
 finger = Finger()
+IGNORE_FINGER = False
 def fingerOKCb(user_id, access):
+	global IGNORE_FINGER
+	if IGNORE_FINGER: return
 	GAME_RUNNING = False
 	clrscrn()
 	lcd.print("Welcome\nUser #"+str(user_id))
@@ -103,13 +111,62 @@ def fingerOKCb(user_id, access):
 	sleep_ms(1000)
 	mainScrn()
 def fingerDENYCb():
+	global IGNORE_FINGER
+	if IGNORE_FINGER: return
 	GAME_RUNNING = False
 	clrscrn()
 	lcd.print("Unknown  :(\nFinger")
 	tone(TONE_DENY)
 	sleep_ms(1000)
-	game_start()
-	reset()
+	clrscrn()
+	lcd.setTextColor(lcd.MAGENTA)
+	lcd.print("Hold button\nto skip\nthe game")
+	sleep_ms(2000)
+	if buttonA.isPressed(): mainScrn()
+	else: game_start()
 finger.readFingerCb(callback=fingerOKCb)
 finger.getUnknownCb(callback=fingerDENYCb)
 
+def enrolCb():
+	global IGNORE_FINGER
+	IGNORE_FINGER = True
+	clrscrn()
+	lcd.setTextColor(lcd.RED)
+	lcd.print("Counting\nRegistered\nUsers")
+	finger.uart.read()
+	finger.uart.write(b'\xf5\t\x00\x00\x00\x00\t\xf5')
+	sleep_ms(50)
+	pkt = finger.uart.read(8)
+	while len(pkt) != 8: pkt = finger.uart.read(8) 
+	print(pkt)
+	if pkt[0] != 0xf5 or pkt[1] != 0x09:
+		clrscrn()
+		tone(TONE_DENY)
+		lcd.print("TRY AGAIN\n:(")
+		sleep_ms(1000)
+		mainScrn()
+	usrid = pkt[3]
+	clrscrn()
+	lcd.setTextColor(lcd.GREEN)
+	lcd.print("Enrol #"+str(usrid)+"\n")
+	lcd.setTextColor(lcd.WHITE)
+	lcd.print("Place Finger\non Sensor")
+	finger.addUser(usrid+1, 1)
+	while not finger.state.startswith("Add user success"):
+		print(finger.state)
+		sleep_ms(200)
+	clrscrn()
+	lcd.setTextColor(lcd.GREEN)
+	lcd.print("Enrol Finger\n\nSUCCESS")
+	IGNORE_FINGER = False
+	sleep_ms(500)
+	mainScrn()
+buttonB.wasReleased(callback=enrolCb)
+
+def mainScrn():
+	clrscrn()
+	lcd.setTextColor(lcd.RED)
+	lcd.print("deLIGHTer\n")
+	lcd.setTextColor(lcd.WHITE)
+	lcd.print("Scan Finger to Unlock")
+mainScrn()
